52a7dd5a2ecd4b2e82e9616d7e454a653140bc55
Barare <74537721+Barare@users.noreply.github.com>
2022-04-24 22:29:27 +0300

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,27 +1,32 @@
 #include "main.h"
-
 /**
- * _strncat - concatenates two strings using most n bytes
- * @dest: string 1
- * @src: strint 2
- * @n: n bytes from from src
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ *_strncat - concatenates two strings
+ *@dest: A pointer to a character that will be changed
+ *@src: A pointer to a character that will be changed
+ *@n: value
+ *Return: dest
  */
-char *_strncat(char *dest, char *src, int n)
-{
-	int i, j, k;
 
-	i = 0, j = 0;
-	while (dest[i] != '\0')
-		i++;
-	while (src[j] != '\0')
-		j++;
-	if (n < j)
-		j = n - 1;
-	for (k = 0; k <= j; k++, i++)
-		dest[i] = src[k];
-	dest[i + 1] = '\0';
-	return (dest);
+char *_strncat(char *dest, char *src, int n)
+
+{
+int i, j;
+
+i = 0;
+while (dest[i] != '\0')
+{
+i++;
+}
+
+j = 0;
+while (j < n && src[j] != '\0')
+{
+dest[i] = src[j];
+j++;
+i++;
+}
+
+dest[i] = '\0';
+
+return (dest);
 }
