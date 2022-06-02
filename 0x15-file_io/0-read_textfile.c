6014156418e2fcf9d07151e09fb2865fa031ede5
Barare <emmanuelbarare@gmail.com>
2022-06-02 19:19:09 +0300

    task zero

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,32 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <fcntl.h>
 #include "main.h"
+#include <unistd.h>
 
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output
- * @filename: pointer to text in a file
- * @letters: number of letters
- * Return: the actual number of letters it could read and print
+ * read_textfile - returns the actual number of letters it could read and print
+ * @filename: file to read and print
+ * @letters: number of letters to print
+ * Return: the number or letters
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    ssize_t file, fread, fwrite;
-    char *totalSize;
 
-    totalSize = malloc(sizeof(char) * letters);
-    if (totalSize == NULL)
-        return (0);
-    if (filename == NULL)
-        return (0);
+	int fd, sz;
+	char *buf;
 
-    file = open(filename, O_RDONLY);
-    if (file == -1)
-        return (0);
-    fread = read(file, totalSize, letters);
-    if (fread == -1)
-        return (0);
-    fwrite = write(STDOUT_FILENO, totalSize, fread);
-    if (fwrite == -1)
-        return (0);
-    close(file);
-    free(totalSize);
-    return (fwrite);
+	if (filename == NULL)
+		return (0);
+	buf = malloc(letters * sizeof(char));
+	if (buf == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	sz = write(STDOUT_FILENO, buf, read(fd, buf, letters));
+	if (sz == -1)
+		return (0);
+	close(fd);
+	free(buf);
+	return (sz);
+
+
 }
